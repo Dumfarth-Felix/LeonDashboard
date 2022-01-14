d88326f1463dc6e4d630f17fe71fdf0aee0e9f63
Dumfarth Felix <f.dumfarth@students.htl-leonding.ac.at>
2022-01-14 19:23:20 +0100

    update to new backend url

--- src/app/back-end.service.ts
+++ src/app/back-end.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import {HttpClient, HttpHeaders} from '@angular/common/http';
 import {Observable} from 'rxjs';
 
-const BASE_URL = 'http://localhost:8080/api';
+const BASE_URL = 'http://leobot.htl-leonding.ac.at:8080/api';
 @Injectable({
   providedIn: 'root'
 })
